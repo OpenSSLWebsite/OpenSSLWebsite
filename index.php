4820554e515ed7ea162a904b4c9952b429e85143
Vincent FILLON <v.fillon.pro@gmail.com>
2015-01-07 10:01:05 +0100

    Ajout formulaire

--- index.php
+++ index.php
@@ -35,8 +35,8 @@
             <div id="header-background"></div>
             <ul class="nav nav-justified nav-pills">
                 <li><a href="index.php?page=PreventionEtInformation_Actualite.tpl"><img src="img/prevention.jpg" class="img-circle img-responsive"></a>Prévention & Information</li>
-                <li><a href="index.php?page=CampagneVaccination.tpl"><img src="img/vaccination.jpg" class="img-circle img-responsive"></a>Campagne de vaccination</li>
-                <li><a href="index.php?page=DeplacementPopulation.tpl"><img src="img/deplacement_pop.jpg" class="img-circle img-responsive"></a>Déplacement de population</li>
+                <li><a href="index.php?page=NewVacciner.tpl"><img src="img/vaccination.jpg" class="img-circle img-responsive"></a>Campagne de vaccination</li>
+                <li><a href="#"><img src="img/deplacement_pop.jpg" class="img-circle img-responsive"></a>Déplacement de population</li>
             </ul>
         </header>
         <div class="container">
