3ee75ea922dd20e970cce60f6bb1400534c7433d
Vincent FILLON <v.fillon.pro@gmail.com>
2015-01-07 11:26:45 +0100

    Animation duration

--- js/global.js
+++ js/global.js
@@ -5,11 +5,11 @@
 
 $( document ).ready(function(){
     $(".container").fadeOut();
-    $("#header-background").css("-webkit-animation", "header-background 3s linear alternate"); //Chrome, Safari, Opera
-    $("#header-background").css("animation", "header-background 3s linear alternate"); //Standard syntax
-    $("header ul li").css("-webkit-animation", "header-buttons 3s linear alternate"); //Chrome, Safari, Opera
-    $("header ul li").css("animation", "header-buttons 3s linear alternate"); //Standard syntax
-    $("#footer-background").css("-webkit-animation", "footer-background 3s linear alternate"); //Chrome, Safari, Opera
-    $("#footer-background").css("animation", "footer-background 3s linear alternate"); //Standard syntax
-    $(".container").delay(2500).fadeIn();
+    $("#header-background").css("-webkit-animation", "header-background 2s linear alternate"); //Chrome, Safari, Opera
+    $("#header-background").css("animation", "header-background 2s linear alternate"); //Standard syntax
+    $("header ul li").css("-webkit-animation", "header-buttons 2s linear alternate"); //Chrome, Safari, Opera
+    $("header ul li").css("animation", "header-buttons 2s linear alternate"); //Standard syntax
+    $("#footer-background").css("-webkit-animation", "footer-background 2s linear alternate"); //Chrome, Safari, Opera
+    $("#footer-background").css("animation", "footer-background 2s linear alternate"); //Standard syntax
+    $(".container").delay(1500).fadeIn();
 });
