66b7fed2ad5d0d0d9ae3038752ad8c1b5a72ba05
Valentin <passe.valentin@gmail.com>
2015-01-15 09:45:36 +0100

    t

--- modeles/newClePrivee.php
+++ modeles/newClePrivee.php
@@ -1,6 +1,6 @@
 <?php
 
-$configars = array("config"=>"C:\Program Files (x86)\EasyPHP-DevServer-14.1VC9\binaries\php\php_runningversion\extras\openssl.cnf");
+$configars = array("config"=>"C:\Program Files (x86)\EasyPHP-DevServer-14.1VC11\binaries\php\php_runningversion\extras\ssl\openssl.cnf");
 $privkey = openssl_pkey_new();
 
 header("Location: ../index.php?page=pictionary.tpl&kprivkey=".$privkey);
